dab1324d45e7d843ef4fe4dc2ccc3eabfe5e63d0
droddor <45990584+droddor@users.noreply.github.com>
2025-04-27 23:18:52 +0200

    Add files via upload

---
+++ E4.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <dirent.h>
+
+int main(int argc, char *argv[]){
+    DIR *dir;
+    struct dirent *mi_dirent;
+    if(argc != 2){
+        printf("%s: %s directorio\n",argv[0], argv[0]);
+        exit(-1);
+    }
+    if((dir = opendir(argv[1])) == NULL){
+        perror("opendir");
+        exit(-1);
+    }
+    while((mi_dirent = readdir(dir)) != NULL)
+        printf("%s\n",mi_dirent->d_name);
+    closedir(dir);
+    return 0;
+}
+
+/*
+Execute with arg:
+
+Relative path or complete:
+
+Relative : ./
+Complete: /documents/dirname
+
+*/
